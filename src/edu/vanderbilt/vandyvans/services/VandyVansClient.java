6af537c85998823e00313c6f5d735095af68b565
AliceCengal <cengallut@gmail.com>
2014-04-13 21:48:22 -0500

    caching

--- src/edu/vanderbilt/vandyvans/services/VandyVansClient.java
+++ src/edu/vanderbilt/vandyvans/services/VandyVansClient.java
@@ -4,12 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Message;
 import android.util.JsonWriter;
@@ -28,7 +31,7 @@ import edu.vanderbilt.vandyvans.models.Stop;
 import static edu.vanderbilt.vandyvans.services.Global.APP_LOG_ID;
 
 /**
- * Implements the requests to the `vandyvans.com` server
+ * Implements the requests to the `vandyvans.com` server.
  *
  * Created by athran on 3/16/14.
  */
@@ -45,11 +48,18 @@ final class VandyVansClient implements Handler.Callback {
     private static final String REPORT_ISBUG     = "isBugReport";
     private static final String REPORT_NOTIFY    = "notifyWhenResolved";
 
+    private static final String ROUTE_CACHE_DATE = "VandyVansClientRouteCacheDate";
+    private static final String ROUTE_DATA       = "VandyVansClientRouteData";
+    private static final String STOPS_CACHE_DATE = "VandyVansClientStopsCacheDate";
+    private static final String STOPS_DATA       = "VandyVansClientStopsData";
+    private static final long   CACHE_EXPIRATION = 14 * 24 * 3600 * 1000;
+
+    private SharedPreferences mPrefs;
 
     @Override
     public boolean handleMessage(Message msg) {
         if (msg.obj instanceof Global.Initialize)
-            return init();
+            return init(((Global.Initialize) msg.obj).ctx);
 
         else if (msg.obj instanceof Global.FetchStops)
             return fetchStops(
@@ -67,68 +77,132 @@ final class VandyVansClient implements Handler.Callback {
         else return false;
     }
 
-    private boolean init() {
+    private boolean init(Context ctx) {
         Log.d(APP_LOG_ID, LOG_TAG + " | Initialization");
+        mPrefs = ctx.getSharedPreferences(Global.APP_PREFERENCES,
+                                           Context.MODE_PRIVATE);
         return true;
     }
 
     private boolean fetchStops(Handler requester, Route r) {
 
-        StringBuilder buffer = new StringBuilder(BASE_URL)
+        String cacheId     = STOPS_DATA + r.id;
+        String dateCacheId = STOPS_CACHE_DATE + r.id;
+
+        if (mPrefs.contains(cacheId)) {
+            if (!isCacheExpired(dateCacheId)) {
+                List<Stop> result = parseStopResult(
+                        new StringReader(
+                                mPrefs.getString(cacheId, "")));
+
+                if (result.isEmpty()) {
+                    invalidateCache(cacheId);
+                    // jump to DO_HTTP_FETCH
+
+                } else {
+                    requester
+                            .obtainMessage(0, new Global.StopResults(result))
+                            .sendToTarget();
+                    return true; // SHORTCUT
+                }
+
+            } else {
+                invalidateCache(cacheId);
+                // jump to DO_HTTP_FETCH
+            }
+        }
+
+        // DO_HTTP_FETCH
+        final StringBuilder buffer = new StringBuilder(BASE_URL)
                 .append("/Route/")
                 .append(r.id)
                 .append("/Direction/0/Stops");
 
         try {
-            Reader reader = new InputStreamReader(Global.get(buffer.toString()));
-            List<Stop> result = new LinkedList<Stop>();
-            for (JsonElement elem : PARSER.parse(reader).getAsJsonArray()) {
-                JsonObject obj = elem.getAsJsonObject();
-                result.add(new Stop(
-                        obj.get(Stop.TAG_ID).getAsInt(),
-                        obj.get(Stop.TAG_NAME).getAsString(),
-                        obj.get(Stop.TAG_IMAGE).getAsString(),
-                        obj.get(Stop.TAG_LAT).getAsDouble(),
-                        obj.get(Stop.TAG_LON).getAsDouble(),
-                        obj.get(Stop.TAG_RTPI).getAsInt()));
-            }
-
+            final Reader reader = new InputStreamReader(Global.get(buffer.toString()));
+            final String rawResult = readAll(reader);
             reader.close();
+
+            final List<Stop> result = parseStopResult(new StringReader(rawResult));
+
             requester
                     .obtainMessage(0, new Global.StopResults(result))
                     .sendToTarget();
 
+            storeCacheRawDataUpdateDate(cacheId, rawResult, dateCacheId);
+
         } catch (Exception e) {
             Log.e(APP_LOG_ID, LOG_TAG + " | Failed to get Stops for Route.");
             Log.e(APP_LOG_ID, LOG_TAG + " | URL: " + buffer.toString());
-            Log.e(APP_LOG_ID, e .getMessage());
+            Log.e(APP_LOG_ID, e.getMessage());
         }
         return true;
     }
 
+    private static List<Stop> parseStopResult(Reader reader) {
+        List<Stop> result = new LinkedList<Stop>();
+
+        for (JsonElement elem : PARSER.parse(reader).getAsJsonArray()) {
+            JsonObject obj = elem.getAsJsonObject();
+            result.add(new Stop(
+                    obj.get(Stop.TAG_ID).getAsInt(),
+                    obj.get(Stop.TAG_NAME).getAsString(),
+                    obj.get(Stop.TAG_IMAGE).getAsString(),
+                    obj.get(Stop.TAG_LAT).getAsDouble(),
+                    obj.get(Stop.TAG_LON).getAsDouble(),
+                    obj.get(Stop.TAG_RTPI).getAsInt()));
+        }
+
+        return result;
+    }
+
     private boolean waypoints(Handler requester, Route r) {
 
+        String cacheId     = ROUTE_DATA + r.id;
+        String cacheDateId = ROUTE_CACHE_DATE + r.id;
+
+        if (mPrefs.contains(cacheId)) {
+            if (!isCacheExpired(cacheId)) {
+                List<FloatPair> result = parseWaypointResult(
+                        new StringReader(
+                                mPrefs.getString(cacheId, "")));
+                if (result.isEmpty()) {
+                    invalidateCache(cacheId);
+                    // jump to DO_HTTP_FETCH
+
+                } else {
+                    requester
+                            .obtainMessage(0,
+                                           new Global.WaypointResults(result))
+                            .sendToTarget();
+                    return true; // SHORTCUT
+                }
+
+            } else {
+                invalidateCache(cacheId);
+                // jump to DO_HTTP_FETCH
+            }
+        }
+
+        // DO_HTTP_FETCH
         StringBuilder buffer = new StringBuilder(BASE_URL)
                 .append("/Route/")
                 .append(r.id)
                 .append("/Waypoints");
 
         try {
-            Reader reader = new InputStreamReader(Global.get(buffer.toString()));
-            List<FloatPair> result = new LinkedList<FloatPair>();
-            for (JsonElement elem : PARSER.parse(reader).getAsJsonArray()) {
-                JsonObject obj = elem.getAsJsonObject();
-                result.add(new FloatPair(
-                        obj.get(FloatPair.TAG_LAT).getAsDouble(),
-                        obj.get(FloatPair.TAG_LON).getAsDouble()));
-            }
-
+            final Reader reader = new InputStreamReader(Global.get(buffer.toString()));
+            final String rawResult = readAll(reader);
             reader.close();
+
+            List<FloatPair> result = parseWaypointResult(new StringReader(rawResult));
 
             requester
                     .obtainMessage(0,
                             new Global.WaypointResults(result))
                     .sendToTarget();
+
+            storeCacheRawDataUpdateDate(cacheId, rawResult, cacheDateId);
 
         } catch (Exception e) {
             Log.e(APP_LOG_ID, LOG_TAG + " | Failed to get Waypoints for Route.");
@@ -136,6 +210,18 @@ final class VandyVansClient implements Handler.Callback {
             Log.e(APP_LOG_ID, e.getMessage());
         }
         return true;
+    }
+
+    private static List<FloatPair> parseWaypointResult(Reader reader) {
+        List<FloatPair> result = new LinkedList<FloatPair>();
+        for (JsonElement elem : PARSER.parse(reader).getAsJsonArray()) {
+            JsonObject obj = elem.getAsJsonObject();
+            result.add(new FloatPair(
+                    obj.get(FloatPair.TAG_LAT).getAsDouble(),
+                    obj.get(FloatPair.TAG_LON).getAsDouble()));
+        }
+
+        return result;
     }
 
     private boolean postReport(Report report) {
@@ -211,6 +297,44 @@ final class VandyVansClient implements Handler.Callback {
         reportObj.put(REPORT_NOTIFY,    report.notifyWhenResolved);
         reportObj.saveInBackground();
         return true;
+    }
+
+    /**
+     * Cache is expired if it was made more than two weeks ago.
+     */
+    private boolean isCacheExpired(String cacheId) {
+        final long currentTime = System.currentTimeMillis();
+        final long cacheDate = mPrefs.getLong(cacheId, currentTime);
+        return (currentTime - cacheDate) > CACHE_EXPIRATION;
+    }
+
+    private void invalidateCache(String cacheId) {
+        mPrefs.edit()
+                .remove(cacheId)
+                .apply();
+    }
+
+    private void storeCacheRawDataUpdateDate(String cacheId, String data, String cacheDateId) {
+        mPrefs.edit()
+                .putString(cacheId, data)
+                .putLong(cacheDateId, System.currentTimeMillis())
+                .apply();
+    }
+
+    private static String readAll(Reader reader) {
+        StringWriter buffer = new StringWriter();
+
+        try {
+            for (int c = reader.read();
+                 c > -1;
+                 c = reader.read()) {
+                buffer.write(c);
+            }
+        } catch (IOException e) {
+            // Let it go!
+        }
+
+        return buffer.toString();
     }
 
 }
